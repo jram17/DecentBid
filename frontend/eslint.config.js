ac810539ab94b2cd4406ff6882f5b020eb4e478c
inthrakumar <inthrak04@gmail.com>
2024-10-11 12:32:42 +0530

    setup shadcn ui library for the project

--- frontend/eslint.config.js
+++ frontend/eslint.config.js
@@ -29,6 +29,8 @@ export default [
       ...react.configs['jsx-runtime'].rules,
       ...reactHooks.configs.recommended.rules,
       'react/jsx-no-target-blank': 'off',
+      'no-undef': 'off',
+      'no-unused-vars': 'off',
       'react-refresh/only-export-components': [
         'warn',
         { allowConstantExport: true },
