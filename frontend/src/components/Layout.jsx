2e7ba04d71060e153fa5696c6600cadcc7e06277
inthrakumar <inthrak04@gmail.com>
2024-10-20 18:14:52 +0530

    setting up layout for the project

--- frontend/src/components/Layout.jsx
+++ frontend/src/components/Layout.jsx
@@ -1,12 +1,16 @@
 import React from 'react';
 import { Outlet } from 'react-router-dom';
 import { Toaster } from '@/components/ui/toaster';
+import Header from './Header/Header';
+import Footer from './Footer/Footer';
 const Layout = () => (
   <div className="flex flex-col min-h-screen max-w-[100vw]">
+    <Header />
     <div className="flex-grow">
       <Outlet />
       <Toaster />
     </div>
+    <Footer />
   </div>
 );
 
--- hardhat/deploy/deploy.js
+++ hardhat/deploy/deploy.js
@@ -4,31 +4,31 @@ const path = require('path');
 const MainDeploy = require('../artifacts/contracts/Main.sol/Main.json');
 async function main() {
     const [deployer] = await ethers.getSigners();
-    
+
     console.log("Deploying contracts with the account:", deployer.address);
-    
+
     const Main = await ethers.getContractFactory("Main");
     const main = await Main.deploy();
     await main.deployed();
-    
+
     console.log(`Main contract deployed at address: ${main.address}`);
-    
+
     const contractDetails = {
         contractAddress: main.address,
         deployerAddress: deployer.address,
         timestamp: new Date().toISOString(),
-        abi:MainDeploy.abi
+        abi: MainDeploy.abi
     };
 
     try {
-        fs.writeFileSync(path.join(__dirname,'../../frontend/contractDetails.json'), JSON.stringify(contractDetails, null, 2));
+        fs.writeFileSync(path.join(__dirname, '../../frontend/contractDetails.json'), JSON.stringify(contractDetails, null, 2));
     } catch (error) {
         console.error('Error writing contract details to file:', error);
     }
 }
 
 main()
-.catch((error) => {
-    console.error(error);
-    process.exitCode = 1;
-});
+    .catch((error) => {
+        console.error(error);
+        process.exitCode = 1;
+    });
