3412a7615ae1d041b9f21c23b461f88c7f394016
inthrakumar <inthrak04@gmail.com>
2024-10-20 22:32:12 +0530

    firbase init for file upload

---
+++ frontend/src/firebase/firebaseconfig.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { envconfig } from "envconfig";
+import { getStorage } from 'firebase/storage';
+const firebaseConfig = {
+    apiKey: envconfig.VITE_FIREBASE_API_KEY,
+    authDomain: envconfig.VITE_FIREBASE_AUTH_DOMAIN,
+    projectId: envconfig.VITE_FIREBASE_PROJECT_ID,
+    storageBucket: envconfig.VITE_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: envconfig.VITE_FIREBASE_MESSAGING_SENDER_ID,
+    appId: envconfig.VITE_FIREBASE_APP_ID,
+    measurementId: envconfig.VITE_FIREBASE_MEASUREMENT_ID
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const storage = getStorage(app);
+
+export { app, storage };
