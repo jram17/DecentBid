5126b612fc754c3b684a22feb60424709fabf2ff
inthrakumar <inthrak04@gmail.com>
2024-11-10 15:56:57 +0530

    set up routes for the user auctions page

--- server/routes/GetAuctionRouter.js
+++ server/routes/GetAuctionRouter.js
@@ -1,7 +1,10 @@
 const router = require('express').Router();
-const { auctionDetails, getAuctions } = require('../controllers/GetAuctionsController');
-router.get('/', getAuctions);
+const { auctionDetails, getAuctions, getOwningAuctions, updatingUserAuctions, userAuctions } = require('../controllers/GetAuctionsController');
 
+router.get('/', getAuctions);
+router.get('/own-auctions/:owner', getOwningAuctions);
+router.put('/user-auctions', updatingUserAuctions);
+router.get('/user-auctions/:address', userAuctions);
 router.get('/:id', auctionDetails);
 
 module.exports = router;
